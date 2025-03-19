d0f39af4c1e2641172caae47606b3c9efc403e9b
asingh866 <asingh866@my.bcit.ca>
2025-03-19 12:29:40 -0700

    test

--- scripts/firebaseAPI_BBY21.js
+++ scripts/firebaseAPI_BBY21.js
@@ -1,24 +1,17 @@
-// Import Firebase modules properly using CDN URLs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCIEt08N8EAKD2AjgGx1VghYx5XKkjipnU",
-  authDomain: "demo10singhashmeet.firebaseapp.com",
-  projectId: "demo10singhashmeet",
-  storageBucket: "demo10singhashmeet.appspot.com",
-  messagingSenderId: "430577309230",
-  appId: "1:430577309230:web:335d186a1b8420166471bf",
-  measurementId: "G-W05EP00RVC",
+  apiKey: "AIzaSyAmvA1dMsJYhw5RWT9AfSyYcqZvoSviWak",
+  authDomain: "comp1800sprint2.firebaseapp.com",
+  projectId: "comp1800sprint2",
+  storageBucket: "comp1800sprint2.firebasestorage.app",
+  messagingSenderId: "735563755343",
+  appId: "1:735563755343:web:b1d624ada64c6fde54df3a",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { auth, db, storage };
